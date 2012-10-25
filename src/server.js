60d39285bd174efb7e589821e7e6559aae7415c0
libra guest <jay@adnx.net>
2012-10-25 10:24:00 -0400

    small code refactoring

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-var port = process.env.PORT || 4444;
+var port = process.env.PORT;//|| 4444;
+var addr = process.env.IP;//|| "127.0.0.1";
 var net = require("net");
 var sys = require("util");
 var Host = require('./adn3.net.host');
@@ -6,7 +7,52 @@ var Channel = require('./adn3.net.channel');
 var u = require('./adn3.net.utils');
 var puts = console.log;
 var inspect = sys.inspect;
+require('./adn3.net.version');
 
+var RqtHandler = function(host) {
+    this.host = host;
+    };
+
+RqtHandler.prototype.onConnect = function() {
+    this.host.message("test", undefined);
+};
+RqtHandler.prototype.onData = function(data) {
+	try {
+		var i;
+        var buffer = "";
+        buffer += data;
+		while (i = buffer.indexOf("\r\n")) {
+			if (i < 0) break;
+			var msg = buffer.slice(0,i);
+			if (msg.length > 1024) {
+				this.host.quit("Too much data");
+			} else {
+				buffer = buffer.slice(i+2);
+				this.host.process(msg);
+			}
+		}
+	} catch(e) {
+		puts("Receive : Exception - " + e);
+	}
+};
+RqtHandler.prototype.onEnd = function(data) {
+	try {
+		if (this.host !== undefined) { 
+			this.host.quit("Connection reset by peer");
+		}
+	} catch (e) {
+		puts("Eof : Exception - " + e);
+	}    
+};
+RqtHandler.prototype.onTimeout = function(data) {
+	try {
+		if (this.host !== undefined) {
+			this.host.quit("idle timeout");
+		}
+	} catch (e) {
+		puts("Timeout : Exception - " + e);
+	}    
+};
 
 var server = net.createServer(function (s) {
     s.setTimeout(2*60*1000); // 2 minutes idle timeout
@@ -14,13 +60,17 @@ var server = net.createServer(function (s) {
 	u.debug("Connection from " + s.remoteAddress);
 	
 	var host = new Host(s);
-	var buffer = "";
 	u.hosts[host.uuid] = host;
+    var rqtHandler = new RqtHandler(host);
 	
-	s.addListener("connect", function () {
-		host.message(projectname, undefined);
+    s.addListener("connect", rqtHandler.onConnect);
+    s.addListener("data", rqtHandler.onData);
+    s.addListener("end",rqtHandler.onEnd);
+    s.addListener("timeout",rqtHandler.onTimeout);
+    /*
+    s.addListener("connect", function () {
+		host.message("test", undefined);
 	});
-	
 	s.addListener("data", function (data) {
 		try {
 			var i;
@@ -39,7 +89,6 @@ var server = net.createServer(function (s) {
 			puts("Receive : Exception - " + e);
 		}
 	});
-
 	s.addListener("end", function (data) {
 		try {
 			if (host !== undefined) { 
@@ -59,8 +108,7 @@ var server = net.createServer(function (s) {
 			puts("Timeout : Exception - " + e);
 		}
 	});
+    */
 });
-
-	server.listen(port);
-	puts("Listening on port : " + port);
-	
+server.listen(port, addr);
+puts("Listening on ["+addr+"] port ["+port+"]");
